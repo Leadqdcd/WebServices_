aaee4082f1fecaf3aa86e039e3d3a4486645142a
LeaDqdcd <lea.queiros@yahoo.fr>
2025-03-18 14:22:55 +0100

    Initial commit

---
+++ REST-MongoDB/init.sql
@@ -0,0 +1,20 @@
+// All other imports here.
+const { MongoClient } = require("mongodb");
+
+const app = express();
+const port = 8000;
+const client = new MongoClient("mongodb://localhost:27017");
+let db;
+
+app.use(express.json());
+
+// Product Schema + Product Route here.
+
+// Init mongodb client connection
+client.connect().then(() => {
+  // Select db to use in mongodb
+  db = client.db("myDB");
+  app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+  });
+});
