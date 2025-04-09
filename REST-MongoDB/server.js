37121fbf2592c257e0e41f8fdfaf56e46dbc967b
LeaDqdcd <lea.queiros@yahoo.fr>
2025-04-09 08:51:31 +0200

    Fix Create Product 1

--- REST-MongoDB/server.js
+++ REST-MongoDB/server.js
@@ -8,7 +8,7 @@ let db;
 
 app.use(express.json());
 
-// Connexion à MongoDB
+// Connexion à MongoDB et exécution des opérations asynchrones
 client.connect().then(async () => {
   db = client.db("myDB");
   const collection = db.collection("documents");
@@ -25,8 +25,22 @@ client.connect().then(async () => {
   const filteredDocs = await collection.find({ a: 3 }).toArray();
   console.log('Found documents filtered by { a: 3 } =>', filteredDocs);
 
+  // Mise à jour d'un document
+  const updateResult = await collection.updateOne({ a: 3 }, { $set: { b: 1 } });
+  console.log('Updated documents =>', updateResult);
+
+  // Suppression d'un document
+  const deleteResult = await collection.deleteMany({ a: 3 });
+  console.log('Deleted documents =>', deleteResult);
+
+  // Création d'un index sur le champ a
+  const indexName = await collection.createIndex({ a: 1 });
+  console.log('Index name =', indexName);
+
   // Lancement du serveur
   app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
   });
+}).catch(error => {
+  console.error("Erreur de connexion à MongoDB :", error);
 });
