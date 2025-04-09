49937fab1e4b036fc57d31e7858236a27b5f7b02
LeaDqdcd <lea.queiros@yahoo.fr>
2025-04-09 09:26:44 +0200

    Fix messsage user

--- Websocket/index.js
+++ Websocket/index.js
@@ -18,3 +18,16 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+  });
